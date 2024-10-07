bd3cdf6de06ab2ce8e8fe5042d40752d3dccb078
packt-simrana <packt.simrana@gmail.com>
2024-10-08 02:46:14 +0530

    Commits

--- obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f504972900b606bb833187f3e34cbc49334580")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
